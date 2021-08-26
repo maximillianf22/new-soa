760d4d1c70a0b0cb0db870223a0c66a12a39d4dc
Didiel <didiel.f@gmail.com>
2021-08-25 20:23:58 -0400

    Inicios cambios de login

--- src/app/modules/auth/AuthPage.tsx
+++ src/app/modules/auth/AuthPage.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, {useEffect} from 'react'
-import {Redirect, Route, Switch} from 'react-router-dom'
-import {Registration} from './components/Registration'
-import {ForgotPassword} from './components/ForgotPassword'
-import {Login} from './components/Login'
+// import {Redirect, Route, Switch} from 'react-router-dom'
+// import {Registration} from './components/Registration'
+// import {ForgotPassword} from './components/ForgotPassword'
+// import {Login} from './components/Login'
 import {toAbsoluteUrl} from '../../../_metronic/helpers'
+import './authPage.css'
+
+
 
 export function AuthPage() {
-  useEffect(() => {
+  useEffect(() => { 
     document.body.classList.add('bg-white')
     return () => {
       document.body.classList.remove('bg-white')
@@ -15,49 +18,95 @@ export function AuthPage() {
   }, [])
 
   return (
-    <div
-      className='d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed'
-      style={{
-        backgroundImage: `url(${toAbsoluteUrl('/media/illustrations/progress-hd.png')})`,
-      }}
-    >
-      {/* begin::Content */}
-      <div className='d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20'>
-        {/* begin::Logo */}
-        <a href='#' className='mb-12'>
-          <img alt='Logo' src={toAbsoluteUrl('/media/logos/logo-2-dark.svg')} className='h-45px' />
-        </a>
-        {/* end::Logo */}
-        {/* begin::Wrapper */}
-        <div className='w-lg-500px bg-white rounded shadow-sm p-10 p-lg-15 mx-auto'>
-          <Switch>
-            <Route path='/auth/login' component={Login} />
-            <Route path='/auth/registration' component={Registration} />
-            <Route path='/auth/forgot-password' component={ForgotPassword} />
-            <Redirect from='/auth' exact={true} to='/auth/login' />
-            <Redirect to='/auth/login' />
-          </Switch>
+
+
+    <div className="container-fluid w-100 container-full-height">
+      <div className="row h-100">
+        <div 
+          className="col-4 bg-dark h-100"
+          style={{
+            backgroundImage: `url(${toAbsoluteUrl('/media/soa/login/addimg.jpg')})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover"
+          }}
+        >
+          <div className="row logo m-5"
+            style={{
+              backgroundImage: `url(${toAbsoluteUrl('/media/soa/login/addiuva-logo.svg')})`,
+            }}
+          >
+          </div>
+          <div className="row"
+          >
+            <p className="text-white text-center display-7 fw-bolder">
+              Ayuda sin Fronteras
+            </p>
+          </div>
+
         </div>
-        {/* end::Wrapper */}
-      </div>
-      {/* end::Content */}
-      {/* begin::Footer */}
-      <div className='d-flex flex-center flex-column-auto p-10'>
-        <div className='d-flex align-items-center fw-bold fs-6'>
-          <a href='#' className='text-muted text-hover-primary px-2'>
-            About
-          </a>
-
-          <a href='#' className='text-muted text-hover-primary px-2'>
-            Contact
-          </a>
-
-          <a href='#' className='text-muted text-hover-primary px-2'>
-            Contact Us
-          </a>
+        <div className="col-8">
+          2
         </div>
       </div>
-      {/* end::Footer */}
     </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <div
+    //   className='d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed'
+    //   style={{
+    //     backgroundImage: `url(${toAbsoluteUrl('/media/illustrations/progress-hd.png')})`,
+    //   }}
+    // >
+    //   {/* begin::Content */}
+    //   <div className='d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20'>
+    //     {/* begin::Logo */}
+    //     <a href='#' className='mb-12'>
+    //       <img alt='Logo' src={toAbsoluteUrl('/media/logos/logo-2-dark.svg')} className='h-45px' />
+    //     </a>
+    //     {/* end::Logo */}
+    //     {/* begin::Wrapper */}
+    //     <div className='w-lg-500px bg-white rounded shadow-sm p-10 p-lg-15 mx-auto'>
+    //       <Switch>
+    //         <Route path='/auth/login' component={Login} />
+    //         <Route path='/auth/registration' component={Registration} />
+    //         <Route path='/auth/forgot-password' component={ForgotPassword} />
+    //         <Redirect from='/auth' exact={true} to='/auth/login' />
+    //         <Redirect to='/auth/login' />
+    //       </Switch>
+    //     </div>
+    //     {/* end::Wrapper */}
+    //   </div>
+    //   {/* end::Content */}
+    //   {/* begin::Footer */}
+    //   <div className='d-flex flex-center flex-column-auto p-10'>
+    //     <div className='d-flex align-items-center fw-bold fs-6'>
+    //       <a href='#' className='text-muted text-hover-primary px-2'>
+    //         About
+    //       </a>
+
+    //       <a href='#' className='text-muted text-hover-primary px-2'>
+    //         Contact
+    //       </a>
+
+    //       <a href='#' className='text-muted text-hover-primary px-2'>
+    //         Contact Us
+    //       </a>
+    //     </div>
+    //   </div>
+    //   {/* end::Footer */}
+    // </div>
   )
 }
