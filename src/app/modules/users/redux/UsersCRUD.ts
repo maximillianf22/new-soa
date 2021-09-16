b6ca0b3ba153051f1af5bd891eda534ca8c05814
Didiel <didiel.f@gmail.com>
2021-09-16 10:59:17 -0300

    axios change

--- src/app/modules/users/redux/UsersCRUD.ts
+++ src/app/modules/users/redux/UsersCRUD.ts
@@ -7,4 +7,16 @@ export const GET_USERS_URL = `/soaang-users/api/users/`;
 // Servidor debe retonar UserModel
 export function getUsers(): Promise<UserModel[]> {
   return httpClient.get(GET_USERS_URL)
-}
+};
+
+export function getUser(id: number): Promise<UserModel[]> {
+  return httpClient.get(`${GET_USERS_URL}/id`)
+};
+
+export function deleteUser(id: number): Promise<UserModel[]> {
+  return httpClient.delete(`${GET_USERS_URL}/id`)
+};
+
+export function updateUser(id: number, user: UserModel): Promise<UserModel[]> {
+  return httpClient.put(`${GET_USERS_URL}/id`, { user })
+};
