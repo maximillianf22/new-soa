df090ba5f0266df12dd599ded6096e6de3bd56b3
maximillianf22 <dgse.informatica@gmail.com>
2021-09-22 11:01:56 -0500

    Funcionalidad
    Se esta trabajando en los catalogos

---
+++ src/app/pages/layout/SelectPlataforms.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Select from 'react-select'
+
+const options = [
+  {value: 'plataforma1', label: 'Plaaforma One'},
+  {value: 'plataforma2', label: 'Plataform Two'},
+  {value: 'plataforma3', label: 'Plataform Three'},
+  {value: 'Plataforma4', label: 'Plataform Four'},
+]
+
+export const SelectPlataforms = () => {
+  return (
+    <>
+      <div className='col-5 ps-4 col-md-4 col-lg-3'>
+        <Select
+          className='form-control form-control-sm form-control-white border-0 p-0'
+          placeholder='Seleccione la Plataforma o Cliente'
+          name='selectPlataform'
+          options={options}
+          id='plataform'
+        />
+      </div>
+    </>
+  )
+}
