d08db49ee7b4e5679f4b31d19316f48091213575
Didiel <didiel.f@gmail.com>
2021-09-28 15:22:35 -0300

    ui state to family

--- src/app/modules/families/components/FamiliesForm.tsx
+++ src/app/modules/families/components/FamiliesForm.tsx
@@ -12,6 +12,8 @@ const optionsGroups = [
 
 export const FamiliesForm = () => {
   const SelectedFamily: any = useSelector<RootState>(({families}) => families.SelectedFamily)
+  const {loading, editing: isEditing, viewing: isViewing}: any = useSelector<RootState>(({ui}) => ui)
+
   const dispatch = useDispatch()
 
   return (
@@ -34,9 +36,7 @@ export const FamiliesForm = () => {
               <div className='card-body'>
                 <div className='row'>
                   <div className='col-md-12 px-5 fv-row my-3'>
-                    {SelectedFamily.toEdit 
-                      ? <InputCustom type='text' name='fmDescription' label='Descripcion' required />
-                      : <InputCustom type='text' name='fmDescription' label='Descripcion' required readOnly />}
+                    <InputCustom type='text' name='fmDescription' label='Descripcion' required disabled={isViewing} />
                   </div>
                   <div className='col-md-12 px-5 fv-row my-3'>
                     <label className='col-form-label required fw-bold fs-6 py-2'>Grupo</label>
@@ -51,26 +51,39 @@ export const FamiliesForm = () => {
                           type='checkbox'
                           name='fmStatus'
                           id='flexSwitchChecked'
+                          disabled={isViewing}
                         />
                         <label className='form-check-label'>¿Habilitado?</label>
                       </div>
                     </div>
                   </div>
                 </div>
-                {SelectedFamily.toEdit 
-                  ? (
-                    <div className='px-5 pt-5 fv-row text-end'>
-                      <button type='submit' className='btn btn-primary' data-bs-dismiss='modal'>
-                        Guardar
+                <div className='px-5 pt-5 fv-row text-end'>
+                  {!isViewing 
+                  ?
+                    <button
+                      type='submit'
+                      className='btn btn-primary'
+                      data-bs-dismiss='modal'
+                      disabled={!props.dirty || !props.isValid}
+                    >
+                      {!loading && <span className='indicator-label'>{isEditing? 'Actualizar': 'Guardar'}</span>}
+                        {loading && (
+                          <span className='indicator-progress' style={{display: 'block'}}>
+                            {isEditing? 'Actualizar': 'Guardar'}
+                            <span className='spinner-border spinner-border-sm align-middle ms-2'></span>
+                          </span>
+                        )}
                       </button>
-                    </div>
-                  ) : (
-                    <div className='px-5 pt-5 fv-row text-end'>
-                      <button type='button' className='btn btn-primary' data-bs-dismiss='modal'>
-                        Cerrar
-                      </button>
-                    </div>
-                  )}
+                    :
+                    <button type='button' className='btn btn-info mx-8'>
+                      Ver servicios
+                    </button>
+                    }
+                  <button type='button' className='btn btn-primary mx-8' data-bs-dismiss='modal'>
+                    Cerrar
+                  </button>
+                </div>
               </div>
             </div>
           </Form>
