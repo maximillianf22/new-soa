a4021718b3cf8e249993a52c2b23448af0f73ea8
Didiel <didiel.f@gmail.com>
2021-09-28 11:25:25 -0300

    family changes

--- src/app/modules/families/components/FamiliesForm.tsx
+++ src/app/modules/families/components/FamiliesForm.tsx
@@ -34,7 +34,9 @@ export const FamiliesForm = () => {
               <div className='card-body'>
                 <div className='row'>
                   <div className='col-md-12 px-5 fv-row my-3'>
-                    <InputCustom type='text' name='fmDescription' label='Descripcion' required />
+                    {SelectedFamily.toEdit 
+                      ? <InputCustom type='text' name='fmDescription' label='Descripcion' required />
+                      : <InputCustom type='text' name='fmDescription' label='Descripcion' required readOnly />}
                   </div>
                   <div className='col-md-12 px-5 fv-row my-3'>
                     <label className='col-form-label required fw-bold fs-6 py-2'>Grupo</label>
@@ -55,11 +57,20 @@ export const FamiliesForm = () => {
                     </div>
                   </div>
                 </div>
-                <div className='px-5 pt-5 fv-row text-end'>
-                  <button type='submit' className='btn btn-primary' data-bs-dismiss='modal'>
-                    Guardar
-                  </button>
-                </div>
+                {SelectedFamily.toEdit 
+                  ? (
+                    <div className='px-5 pt-5 fv-row text-end'>
+                      <button type='submit' className='btn btn-primary' data-bs-dismiss='modal'>
+                        Guardar
+                      </button>
+                    </div>
+                  ) : (
+                    <div className='px-5 pt-5 fv-row text-end'>
+                      <button type='button' className='btn btn-primary' data-bs-dismiss='modal'>
+                        Cerrar
+                      </button>
+                    </div>
+                  )}
               </div>
             </div>
           </Form>
