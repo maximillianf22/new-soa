7147b625e114cb211fb3038dbd4096246a7058e2
maximillianf22 <dgse.informatica@gmail.com>
2021-09-28 08:31:06 -0500

    componentes detalle de cuenta

--- src/app/modules/accounts/components/AccountsView/AccountsDetails.tsx
+++ src/app/modules/accounts/components/AccountsView/AccountsDetails.tsx
@@ -1,31 +1,28 @@
 import React from 'react'
+import {IAccountsDetails} from '../../Interfaces/models'
+import {AccountDetailsHeader} from './AccountDetailsHeader'
 
-export const AccountsDetails = () => {
+export const AccountsDetails = ({
+  vip,
+  active,
+  description,
+  pilot,
+  pilot_provider,
+  count_vip,
+  count_active,
+  count_inactive,
+}: IAccountsDetails) => {
   return (
     <>
       <div className='card'>
         <div className='card-body p-0'>
-          <div className='card-header bg-primary ribbon ribbon-top border-0 min-h-50px'>
-            <div className='ribbon-label bg-success fw-bolder'>Activo</div>
-            <div className='card-title text-white fw-bolder fs-4'>Detalle de la cuenta</div>
-          </div>
-          <div className={`px-9 pt-1 h-200px w-100 bg-primary`}>
-            <div className='d-flex text-center flex-column text-white pt-0'>
-              <span className='fw-bolder fs-1 pt-1 text-nowrap'>Descripcion de la cuenta</span>
-              <div className='row mt-4'>
-                <div className='col-md-6 text-start'>
-                  <span className='fs-9 fw-bold text-uppercase'>Piloto</span>
-                  <br />
-                  <span className='fw-bold fs-4'>308567098</span>
-                </div>
-                <div className='col-md-6 text-end'>
-                  <span className='fs-8 fw-bold text-uppercase'>Proveedor</span>
-                  <br />
-                  <span className='fw-bold fs-4'>308567098</span>
-                </div>
-              </div>
-            </div>
-          </div>
+          <AccountDetailsHeader
+            {...(vip ? {vip} : '')}
+            {...(active ? {active} : '')}
+            description={description}
+            pilot_provider={pilot_provider}
+            pilot={pilot}
+          />
           <div
             className='shadow-xs card-rounded mx-9 mb-9 p-3 position-relative z-index-1 bg-white'
             style={{marginTop: '-80px'}}
@@ -45,7 +42,7 @@ export const AccountsDetails = () => {
                     <div className='text-gray-400 fw-bold fs-7'>Listado</div>
                   </div>
                   <div className='d-flex align-items-center'>
-                    <div className='fw-bolder fs-5 text-warning pe-1'>25</div>
+                    <div className='fw-bolder fs-5 text-warning pe-1'>{count_vip}</div>
                   </div>
                 </div>
               </a>
@@ -65,7 +62,7 @@ export const AccountsDetails = () => {
                     <div className='text-gray-400 fw-bold fs-7'>Listado</div>
                   </div>
                   <div className='d-flex align-items-center'>
-                    <div className='fw-bolder fs-5 text-gray-800 pe-1'>25</div>
+                    <div className='fw-bolder fs-5 text-gray-800 pe-1'>{count_active}</div>
                   </div>
                 </div>
               </a>
@@ -85,7 +82,7 @@ export const AccountsDetails = () => {
                     <div className='text-gray-400 fw-bold fs-7'>Listado</div>
                   </div>
                   <div className='d-flex align-items-center'>
-                    <div className='fw-bolder fs-5 text-gray-800 pe-1'>25</div>
+                    <div className='fw-bolder fs-5 text-gray-800 pe-1'>{count_inactive}</div>
                   </div>
                 </div>
               </a>
